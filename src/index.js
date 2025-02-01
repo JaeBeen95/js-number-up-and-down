23b4a1a9b7fda4d585cc9bd5025b07fea7beeb80
JaeBeen95 <nin2348@gmail.com>
2025-02-01 21:39:24 +0900

    1. 랜덤 숫자 생성

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { readLineAsync } from "./input.js";
 
+const randomNumber = Math.floor(Math.random() * 50) + 1;
+
 async function play() {
   console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
   const inputValue = await readLineAsync("숫자 입력: ");
   console.log(inputValue);
 }
-
-play();
