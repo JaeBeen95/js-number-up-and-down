8304ee30e4ce364a9a4a99099c6445abf45ddcc6
JaeBeen95 <nin2348@gmail.com>
2025-02-01 21:49:46 +0900

    4. 사용자 입력에 대한 유효성 검사 및 통과 못할 시 다시 입력하도록 구현

--- src/index.js
+++ src/index.js
@@ -5,11 +5,29 @@ const gameState = {
   attempts: 0,
 };
 
+const validateInputValue = (inputValue) => {
+  const inputNumber = Number(inputValue);
+  const isNotNumber = Number.isNaN(inputNumber);
+  const isNotInteger = !Number.isInteger(inputNumber);
+  const isOutOfRange = inputNumber < 1 || inputNumber > 50;
+
+  if (isNotNumber || isNotInteger || isOutOfRange) {
+    console.log("1에서 50사이의 숫자 값만 입력해주세요");
+    return false;
+  }
+
+  return true;
+};
+
 async function play() {
   console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
 
   while (true) {
     const inputValue = await readLineAsync("숫자 입력: ");
+
+    if (!validateInputValue(inputValue)) {
+      continue;
+    }
 
     gameState.attempts++;
 
