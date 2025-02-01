7fbb54354c79f8bc19a317e2493cb411e0d1b9c1
JaeBeen95 <nin2348@gmail.com>
2025-02-01 21:46:59 +0900

    3. 사용자 입력 받기 구현

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ const gameState = {
 
 async function play() {
   console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
-  const inputValue = await readLineAsync("숫자 입력: ");
-  console.log(inputValue);
+
+  while (true) {
+    const inputValue = await readLineAsync("숫자 입력: ");
+
+    gameState.attempts++;
+
+    const userNumber = Number(inputValue);
+
+    if (userNumber === gameState.randomNumber) {
+      console.log(`정답! ${gameState.attempts}번 만에 숫자를 맞추셨습니다.`);
+      break;
+    } else if (userNumber > gameState.randomNumber) {
+      console.log("다운");
+    } else {
+      console.log("업");
+    }
+  }
 }
