0b82aa094deb65bb3f0c205d4c29f87981432084
JaeBeen95 <nin2348@gmail.com>
2025-02-01 19:45:55 +0900

    프로젝트 시작

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { readLineAsync } from "./input.js";
+
+async function play() {
+	console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
+  const inputValue = await readLineAsync("숫자 입력: 7");
+  console.log(inputValue);
+}
+
+play()
