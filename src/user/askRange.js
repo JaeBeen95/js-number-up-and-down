830ea59984cb89853658b39b3724ffe5338e5e8d
JaeBeen95 <nin2348@gmail.com>
2025-03-04 21:46:00 +0900

    ask함수 공통 로직 분리

--- src/user/askRange.js
+++ src/user/askRange.js
@@ -1,17 +1,8 @@
-import readLineAsync from "./input.js";
-import { validateRange } from "../utils/index.js";
+import { validateRange, getValidatedInput } from "../utils/index.js";
 
 async function askRange() {
-  while (true) {
-    try {
-      console.log("[게임 설정] 게임 시작을 위해 최소 값, 최대 값을 입력해주세요. (예: 1, 50)");
-      const rangeInput = await readLineAsync("숫자 입력: ");
-      const { min, max } = validateRange(rangeInput);
-      return { min, max };
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+  console.log("[게임 설정] 게임 시작을 위해 최소 값, 최대 값을 입력해주세요. (예: 1, 50)");
+  return getValidatedInput("숫자 입력: ", validateRange);
 }
 
 export default askRange;
